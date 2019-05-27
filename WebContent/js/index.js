ac87b007e0f2011b6542cdb40406576f2755131e
caioxcezar <caioxcezar@gmail.com>
2019-05-27 01:06:15 -0300

    Paginas em JSF
    Adicionei o template master e comecei a pagina index

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,21 +1,22 @@
 let login = document.getElementById("login")
 let header = document.getElementById("navbarSupportedContent")
-let form =  document.getElementById("formulario")
+let form = document.getElementById("formLogin")
 login.onclick = () => {
-    let altura = form.style.top.substring(0, 2)
-    form.style.top = ( altura > 0 ? (-1 * form.offsetHeight)+"px" : "120px")
+  let altura = form.style.top.substring(0, 2)
+  form.style.top = (altura > 0 ? (-1 * form.offsetHeight) + "px" : (header.offsetHeight + 65) + "px")
 }
 $('.destaques').slick({
-    arrows: false,
-    //dots: true,
-    infinite: true,
-    speed: 50,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
-  });
-  $('.maisvendidos').slick({
-	  infinite: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 3
-	});
+	arrows: false,
+	centerMode: true,
+	centerPadding: "10%",
+  slidesToShow: 1,
+  autoplay: false,
+  autoplaySpeed: 2000
+})
+$('.maisvendidos').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerMode: true,
+  variableWidth: false
+})
--- src/Utils/Html.java
+++ src/Utils/Html.java
@@ -3,35 +3,65 @@ package Utils;
 import Models.Jogo;
 
 public class Html {
-	public static String slickCarousel(int[] ids, String nome) {
-		String resultado = "<div class='container " + nome + "'>";
+	public static String destaques(int[] ids) {
+		String resultado = "<div class=\"destaques\">";
 		for(int id:ids) {
 			Jogo j = new Jogo();
 			try {
 				j = JsonUtils.getJogo(id);
-				resultado += "<div class='card mr-2'>" + 
-						"<img class='card-img-top' src='" + j.getImagemCabecalho() + "' alt=''>"
-						+ "<div class='card-body text-dark'>"
-						+ "<h5 class='card-title'>" + j.getNome() + "</h5>"
-						+ "<p class='card-text'>Preço:" + j.getPreco().getFinalFormatado() + "</p>"
-						+ "<a href='/busca.jsp?appid=" + id + "' class='btn btn-primary'>Comprar</a>"
-						+ "</div>"
-						+ "</div>";
+				resultado += "		<div class=\"card mb-3\">\r\n" + 
+						"			<div class=\"row no-gutters\">\r\n" + 
+						"				<div class=\"col-md-8\">\r\n" + 
+						"					<img src=\"" + j.getImagemCabecalho() + "\" class=\"card-img\">\r\n" + 
+						"				</div>\r\n" + 
+						"				<div class=\"col-md-4\">\r\n" + 
+						"					<div class=\"card-body text-white\">\r\n" + 
+						"						<h5 class=\"card-title\">" + j.getNome() +"</h5>\r\n" + 
+						"						<p>Preço: " + j.getPreco().getFinalFormatado() + "</p>\r\n" + 
+						"					</div>\r\n" + 
+						"				</div>\r\n" + 
+						"			</div>\r\n" + 
+						"		</div>";
 			} catch (Exception e) {
-				resultado += "<div class='card mr-2'>"
-						+ "<div class='card-img-top text-center'>"
-						+ "<i class='far fa-dizzy text-dark'></i>"
-						+ "</div>"
-						+ "<div class='card-body text-dark'>"
-						+ "<h5 class='card-title'>Ocorreu um erro</h5>"
-						+ "<p class='card-text'>" + e.getMessage() + "</p>"
-						+ "<a href='/404/index.html' class='btn btn-primary'>Comprar</a>"
-						+ "</div>"
-						+ "</div>";
+				resultado += cardError(e);
 				e.getStackTrace();
 			}
 		}
 		resultado += "</div>";
 		return resultado;
+	}
+	public static String slickCard(int[] ids, String nome) {
+		String resultado = "<div class='container " + nome + "'>";
+		for(int id:ids) {
+			Jogo j = new Jogo();
+			try {
+				j = JsonUtils.getJogo(id);
+				resultado += "<div class='card mr-2'>"
+						+ "<img class='card-img-top' src='" + j.getImagemCabecalho() + "' alt=''>"
+						+ "<div class='card-body text-dark'>"
+						+ "<h5 class='card-title'>" + j.getNome() + "</h5>"
+						+ "<div class='card-text'>Preço:" + j.getPreco().getFinalFormatado() + "</div>"
+						+ "<a href='/busca.jsp?appid=" + id + "' class='btn btn-primary'>Comprar</a>"
+						+ "</div>"
+						+ "</div>";
+			} catch (Exception e) {
+				resultado += cardError(e);
+				e.getStackTrace();
+			}
+		}
+		resultado += "</div>";
+		return resultado;
+	}
+	public static String cardError(Exception e) {
+		return "<div class='card mr-2'>"
+				+ "<div class='text-center'>"
+				+ "<i class='far fa-dizzy text-dark card-img-top'></i>"
+				+ "</div>"
+				+ "<div class='card-body text-dark'>"
+				+ "<h5 class='card-title'>Ocorreu um erro</h5>"
+				+ "<div class='card-text'>" + e.getMessage() + "</div>"
+				+ "<a href='/404/index.html' class='btn btn-primary'>Comprar</a>"
+				+ "</div>"
+				+ "</div>";
 	}
 }
